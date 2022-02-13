3be9be429e327323f3b61e6803dd3e64554143a8
temitope <83991100+aladesiun@users.noreply.github.com>
2022-02-14 05:11:53 +1300

    Update avatarmodal.vue

--- assessment/src/components/avatarmodal.vue
+++ assessment/src/components/avatarmodal.vue
@@ -7,11 +7,11 @@
 >
     Toggle modal
 </button>
-
 <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="popup-modal" aria-hidden="true">
     <div class="relative px-4 w-full max-w-md h-full md:h-auto">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div class="flex justify-end p-2">
+                <notification/>
                 <button
                     type="button"
                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
@@ -30,7 +30,14 @@
                 <form action="" @submit.prevent="uploadavatar">
                     <p class="text-lg">%100</p>
                     <input type="file" @change="avatarFile" accept="avatar/*" class="border my-2 rounded" >
-                    <input type="submit" multiple value="upload" class="bg-fundallgreen border border-fundallgreen text-white py-1 px-4 rounded cursor-pointer hover:bg-white hover:text-fundallgreen hover:border">
+                    <button disabled v-if="loading" type="button" class="bg-fundallgreen border border-fundallgreen text-white py-1 px-4 rounded cursor-pointer hover:bg-white hover:text-fundallgreen hover:border">
+                  <svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                  </svg>
+                  
+              </button>
+                    <input v-else type="submit" multiple value="upload" class="bg-fundallgreen border border-fundallgreen text-white py-1 px-4 rounded cursor-pointer hover:bg-white hover:text-fundallgreen hover:border">
                 </form>
             </div>
         </div>
@@ -41,11 +48,14 @@
 </template>
 
 <script>
+import notification from './notification.vue';
 export default {
+  components: { notification },
     data() {
         return {
             selectedavatar:null,
-            selectedavatar_name:null
+            selectedavatar_name:null,
+            loading:false
 
        }
     },
@@ -56,7 +66,7 @@ export default {
             console.log(this.selectedavatar=e.target.files[0]);
         },
         uploadavatar(){
-
+            this.loading = true
             const avatar =  new FormData();
             avatar.append('avatar', this.selectedavatar)
             this.$store.dispatch('post', {
@@ -66,7 +76,15 @@ export default {
             .then(res =>{
                 const newavatar=res.data.success.url;
                 localStorage.setItem('newavatar', newavatar)
+                this.loading = false
+                this.$store.commit('setNotification',{type:1, message: 'success'})
+
                 window.location.href='/expenses';
+
+            }).catch((error)=>{
+                this.loading = false
+                this.$store.commit('setNotification',{type:2, message: error})
+
             })
         }
     },
